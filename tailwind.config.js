918db364e079b4254f186cba969a0369217f52d8
OPSRv <opsrv1@gmail.com>
2023-01-11 21:22:07 +0200

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      black: "#000000",
+      "black-dark": "#111111",
+      "gray-dark": "#171717",
+      "gray-light": "#2E2E2E",
+      gray: "#1D1D1D",
+      yellow: "#E3B748",
+      white: "#fff",
+      silver: "#E6E6E6",
+      transparent: "transparent",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      body: ['"Inter"'],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
     extend: {},
   },
   plugins: [],
